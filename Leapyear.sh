22d909e6d8b79187cd93f2171f7c08c1b5172ab7
Daniel Piatkovsky <danielp15864@gmail.com>
2023-08-21 15:30:47 +0300

    Committing

--- Leapyear.sh
+++ Leapyear.sh
@@ -5,16 +5,41 @@ leapyear() {
     (( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
 }
 
-Dayoftheyear() {
+year() {
     local year=$1
-
+    local month=$2
+    local day=$3
+    if (( month < 1 || month > 12 || day < 1 || day > 31 )); then
+    echo "None"
+    return 0
+    fi
+    
+    days_of_the_month=("31" "28" "31" "30" "31" "30" "31" "31" "30" "31" "30" "31")
+    if leapyear $year; then
+    days_of_the_month[1]=29
+    fi
+if (( days_of_the_month[month-1] < day )); then
+     echo "None"
+     return 0
+     fi
+     
+     
+    echo "${days_of_the_month[month-1]}"
+    
     if leapyear $year; then
    echo "Leap year"
 else
 echo "Common year"
+  
    fi
+sum=0
+   for (( i=0; i<month-1; i++ )); do
+        (( sum+=days_of_the_month[i] ))
+    done
 
+    (( sum+=day ))
+    echo "$sum" 
 }
 
-Dayoftheyear "$1" "$2"
+year "$1" "$2" "$3"
 
